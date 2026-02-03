b386d112e3d23a5dd1e3a43858ac1d30d53c3a88
Teodor-Kostov <teodorkostov88@gmail.com>
2026-02-03 16:00:58 +0100

    Create base components and add some styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CreatePostComponent } from './create-post/create-post.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CreatePostComponent],
+  imports: [RouterOutlet, CreatePostComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
