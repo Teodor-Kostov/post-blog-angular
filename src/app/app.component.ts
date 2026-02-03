cd231e4c0874b1bdf0a55fd8d598162b88ddefa4
Teodor-Kostov <teodorkostov88@gmail.com>
2026-02-03 15:35:18 +0100

    create Angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CreatePostComponent } from './create-post/create-post.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CreatePostComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'post-blog';
