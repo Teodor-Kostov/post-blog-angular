f10bcc852248bfecaa63bd0ba0a9e270aaae6bf3
Teodor-Kostov <teodorkostov88@gmail.com>
2026-03-18 12:22:44 +0100

    Refactor main component

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import { PostsListComponent } from '../post/posts-list/posts-list.component'; 
 import { ThemeListComponent } from '../theme/theme-list/theme-list.component';
+import { WelcomeMessageComponent } from '../shared/welcome-message/welcome-message.component';
+import { UserService } from '../user/user.service';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [PostsListComponent, ThemeListComponent ],
+  imports: [PostsListComponent, ThemeListComponent, WelcomeMessageComponent ],
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css'],
   host: { style: 'display: block; width: 100%;' }
 })
 export class MainComponent {
+
+  get isLoggedIn(){
+    return this.userService.isLogged;
+  }
+
+  constructor(private userService: UserService){}
 
 }
